81068513cab831dc10762c64606cbbc979f5fedd
Nate Vack <njvack@freshforever.net>
2012-06-15 17:09:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distribute_setup import use_setuptools
 use_setuptools()
-from distutils.core import setup
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='pfile-tools',
     version='0.1dev',
     packages=['pfile_tools',],
     license='BSD License',
-    long_description=open('README.textile').read(),
+    long_description=read('README.textile'),
 )
