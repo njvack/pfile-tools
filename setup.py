b1bbea10e8bb69edba06ac353f49041df515b78c
Nate Vack <njvack@freshforever.net>
2012-06-15 10:21:37 -0500

    Boilerplate

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pfile-tools',
+    version='0.1a',
+    packages=['pfile_tools',],
+    license='BSD License',
+    long_description=open('README.textile').read(),
+)
