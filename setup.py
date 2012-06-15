7df0783f634635539b4094acd6f6c9f4a1a86953
Nate Vack <njvack@freshforever.net>
2012-06-15 11:03:13 -0500

    Let's use distribute!

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
+from distribute_setup import use_setuptools
+use_setuptools()
 from distutils.core import setup
 
 setup(
     name='pfile-tools',
-    version='0.1a',
+    version='0.1dev',
     packages=['pfile_tools',],
     license='BSD License',
     long_description=open('README.textile').read(),
