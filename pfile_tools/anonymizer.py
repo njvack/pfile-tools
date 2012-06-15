40658441e3a968f0229397e1e13f71a5ad62e54e
Nate Vack <njvack@freshforever.net>
2012-06-15 17:16:21 -0500

    Add anonymizer and struct_utils

---
+++ pfile_tools/anonymizer.py
@@ -0,0 +1,50 @@
+# Part of the pfile-tools package
+# Copyright (c) 2012, Board of Regents of the University of Wisconsin
+# Written by Nathan Vack <njvack@wisc.edu>
+# A library for performing anonymization of GE p-file headers.
+
+from collections import namedtuple
+
+import struct_utils
+import logging
+logger = logging.getLogger(__name__)
+
+# option_name and description are for the command-line tool
+AnonEntry = namedtuple("AnonEntry",
+    ["key", "value", "option_name", "description"])
+
+# Note: option_name should be unique in the list.
+DEFAULT_LIST = [
+    AnonEntry("patient_name", "ANONYMIZED", "name", "patient name"),
+    AnonEntry("patient_id", "ANONYMIZED", "id", "patient ID"),
+    AnonEntry("date_of_birth", "", "dateofbirth", "date of birth"),
+    AnonEntry("patient_age", 0, "age", "age"),
+    AnonEntry("patient_weight_g", 0, "weight", "weight"),
+    AnonEntry("patient_sex", 0, "sex", "sex"),
+]
+
+
+class Anonymizer(object):
+
+    def __init__(self, anonymization_list=DEFAULT_LIST):
+        """Creates a new Anonymizer.
+
+        Arguments:
+        anonymization_list -- a list of objects specifying what attributes
+            are to anonymized, and how. Objects must contain "key" and "value"
+            properties. Keys are dotted string struct value identifiers.
+            See DEFAULT_LIST for examples. Defaults to DEFAULT_LIST.
+        """
+        self.anonymization_list = anonymization_list
+
+    def anonymize(self, header):
+        """Runs through self.anonymization_list and anonymizes the header
+        in place.
+
+        Arguments:
+        header -- a pfile header. NOTE: This structure will be modified
+            in place!
+        """
+        for entry in self.anonymization_list:
+            logger.debug("Setting %s to %s" % (entry.key, entry.value))
+            struct_utils.set_struct_value(header, entry.key, entry.value)
---
+++ pfile_tools/struct_utils.py
@@ -0,0 +1,65 @@
+# Part of the pfile-tools package
+# Copyright (c) 2012, Board of Regents of the University of Wisconsin
+# Written by Nathan Vack <njvack@wisc.edu>
+# Some utilities for working with ctypes Structures
+
+import ctypes
+from collections import namedtuple
+
+StructInfo = namedtuple("StructInfo",
+    ["label", "depth", "value", "field_type", "size", "offset"])
+
+
+def dump_struct(struct, include_structs=False):
+    """
+    Recursively travels through a ctypes.Structure and returns a list of
+    namedtuples, containing label, depth, value, size, and offset.
+    If include_structs is true, output will include lines for individual
+    structures and their sizes and offsets -- not just non-structure fields.
+    """
+    output = []
+    _dump_struct_rec(struct, output, include_structs)
+    return output
+
+
+def _dump_struct_rec(
+    struct, output, include_structs=False, prefix='', depth=0, base_offset=0):
+    """
+    Internal recursive method for dumping structures.
+    Appends to the "output" parameter.
+    """
+    struct_class = type(struct)
+    if include_structs:
+        output.append(StructInfo(
+            "%s (%s)" % (prefix, struct_class.__name__),
+            depth, '', str(struct_class), ctypes.sizeof(struct_class), base_offset))
+    for f in struct._fields_:
+        name = f[0]
+        field_type = f[1]
+        field_meta = getattr(struct_class, name)
+        field = getattr(struct, name)
+        cur_prefix = "%s%s." % (prefix, name)
+        field_offset = base_offset + field_meta.offset
+        if isinstance(field, ctypes.Structure):
+            _dump_struct_rec(field, output, include_structs, cur_prefix,
+                depth+1, field_offset)
+        else:
+            label = prefix+name
+            output.append(StructInfo(
+                label, depth, field, field_type.__name__, field_meta.size, field_offset))
+
+def set_struct_value(struct, field_name, value):
+    """
+    Sets a value in a ctypes struct, by dotted struct name. Example:
+    set_header_value(header, "rdb_hdr_exam.patnameff", "foo")
+    will set header.rdb_hdr_exam.patnaneff = "foo"
+    """
+    parts = field_name.split(".")
+    sh = struct
+    while len(parts) > 1:
+        subhead_key = parts[0]
+        parts = parts[1:]
+        sh = getattr(sh, subhead_key)
+    setattr(sh, parts[0], value)
+
+
