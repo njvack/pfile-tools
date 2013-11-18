3f72b358d3c8cbf0d063f2dfc0dfd94fa4e4393c
Nate Vack <njvack@wisc.edu>
2013-11-18 13:29:49 -0600

    Add support for WTF fields in r16 headers
    They seem to have patient data; let's allow
    anonymization of them.
    
    Also, now we check to see if a field appears in
    a header before we try to anonymize it, so we
    can get reasonable debugging messages.

--- pfile_tools/anonymizer.py
+++ pfile_tools/anonymizer.py
@@ -15,12 +15,14 @@ AnonEntry = namedtuple("AnonEntry",
 
 # Note: key and option_name should be unique in the list.
 DEFAULT_LIST = [
-    AnonEntry("patient_name",    "ANONYMIZED", "name",        "patient name"),
-    AnonEntry("patient_id",      "ANONYMIZED", "id",          "patient ID"),
-    AnonEntry("date_of_birth",    "",          "dateofbirth", "date of birth"),
-    AnonEntry("patient_age",      0,           "age",         "age"),
-    AnonEntry("patient_weight_g", 0,           "weight",      "weight"),
-    AnonEntry("patient_sex",      0,           "sex",         "sex"),
+    AnonEntry("patient_name",     "ANONYMIZED", "name",        "patient name"),
+    AnonEntry("patient_id",       "ANONYMIZED", "id",          "patient ID"),
+    AnonEntry("patient_name_2",   "ANONYMIZED", "name_2",      "patient name 2"),
+    AnonEntry("patient_id_2",     "ANONYMIZED", "id_2",        "patient ID 2"),
+    AnonEntry("date_of_birth",    "",           "dateofbirth", "date of birth"),
+    AnonEntry("patient_age",      0,            "age",         "age"),
+    AnonEntry("patient_weight_g", 0,            "weight",      "weight"),
+    AnonEntry("patient_sex",      0,            "sex",         "sex"),
 ]
 
 
@@ -45,6 +47,10 @@ class Anonymizer(object):
         header -- a pfile header. NOTE: This structure will be modified
             in place!
         """
+        logger.debug("Working with a %s" % (type(header)))
         for entry in self.anonymization_list:
-            logger.debug("Setting %s to %s" % (entry.key, entry.value))
-            struct_utils.set_struct_value(header, entry.key, entry.value)
+            if struct_utils.has_struct_value(header, entry.key):
+                logger.debug("Setting %s to %s" % (entry.key, entry.value))
+                struct_utils.set_struct_value(header, entry.key, entry.value)
+            else:
+                logger.debug("%s not found in header" % entry.key)
