ab9b003174fdec512f32e82a8a90703893304c44
Nate Vack <njvack@wisc.edu>
2017-03-06 14:24:20 -0600

    Tweak setup.py, bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python
-import setuptools
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
-import os
-
-import pfile_tools
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name='pfile-tools',
-    version=pfile_tools.VERSION,
-    packages=setuptools.find_packages(),
-    data_files=[('', ['distribute_setup.py'])],
+    version='0.3.2',
+    author='Nathan Vack',
+    author_email='njvack@wisc.edu',
     license='BSD License',
-    long_description=read('README'),
-    url="https://github.com/njvack/pfile-tools",
-    author="Nathan Vack",
-    author_email="njvack@wisc.edu",
-    entry_points = {
+    url='https://github.com/njvack/pfile-tools',
+    packages=['pfile_tools'],
+    entry_points={
         'console_scripts': [
             'dump_pfile_header = pfile_tools.scripts.dump_pfile_header:main',
             'anonymize_pfile = pfile_tools.scripts.anonymize_pfile:main'
-        ]
-    }
-)
+        ]}
+    )
+
+
+
+
+# setup(
+#     name='pfile-tools',
+#     version=pfile_tools.VERSION,
+#     packages=setuptools.find_packages(),
+#     data_files=[('', ['distribute_setup.py'])],
+#     license='BSD License',
+#     long_description=read('README'),
+#     url="https://github.com/njvack/pfile-tools",
+#     author="Nathan Vack",
+#     author_email="njvack@wisc.edu",
+#     entry_points = {
+#         'console_scripts': [
+#             'dump_pfile_header = pfile_tools.scripts.dump_pfile_header:main',
+#             'anonymize_pfile = pfile_tools.scripts.anonymize_pfile:main'
+#         ]
+#     }
+# )
